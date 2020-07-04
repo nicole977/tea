352f8c88fb7f6f521ca1265f8d34279003218ec5
nicole977 <670444951@qq.com>
2020-07-04 14:54:46 +0800

    22

--- src/com/cn/ceshi.java
+++ src/com/cn/ceshi.java
@@ -1,7 +1,9 @@
 package com.cn;
 
 public class ceshi {
-public static void main(String[] args) {
-	System.out.println("11haha412213");
-}
+	public static void main(String[] args) {
+		System.out.println("11haha412213");
+		System.out.println("haha42213");
+		System.out.println("haha42213");
+	}
 }
